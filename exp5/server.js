ca82ad2dd34cd66414bf76aaf8e6466637aebf6e
SolomonSanjiv <solomonsanjiv.8888@gmail.com>
2025-02-24 17:41:44 +0530

    Update server.js

--- exp5/server.js
+++ exp5/server.js
@@ -1,30 +1,40 @@
 const express=require('express');
 const app=express();
+
 app.use(express.json());
+
 app.get('/',(req,res)=>{
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname+'/index.html');
 });
+
 app.get('/login',(req,res)=>{
-    res.sendFile(__dirname + '/login.html');
+    res.sendFile(__dirname+'/login.html');
 });
+
 app.get('/register',(req,res)=>{
-    res.sendFile(__dirname + '/register.html');
+    res.sendFile(__dirname+'/register.html');
 });
-app.get('welcome',(req,res)=>{
-    res.sendFile(__dirname + '/welcome.html');
+
+app.get('/welcome',(req,res)=>{
+    res.sendFile(__dirname+'/welcome.html');
 });
+
 app.post('/',(req,res)=>{
-    res.redirect("/");
+    res.redirect('/');
 });
+
 app.post('/login',(req,res)=>{
-    res.redirect("/login");
+    res.redirect('/login');
 });
+
 app.post('/register',(req,res)=>{
-    res.redirect("/register");
+    res.redirect('/register');
 });
+
 app.post('/welcome',(req,res)=>{
-    res.redirect("/welcome");
+    res.redirect('/welcome');
 });
+
 app.listen(3000,()=>{
-    console.log('our express server is up on port 3000');
+    console.log("Our express server is up on port 3000");
 });
