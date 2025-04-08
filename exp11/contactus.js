40ecd261a38dbdc9170777d3e2342936ab85ff0d
SolomonSanjiv <solomonsanjiv.8888@gmail.com>
2025-04-08 10:31:31 +0530

    Add files via upload

---
+++ exp11/contactus.js
@@ -0,0 +1,4 @@
+const ContactUs=()=>{
+    return <h1>CMRIT,Kandlakoya,Medchal,HYD</h1>
+};
+export default ContactUs;
---
+++ exp11/profile.js
@@ -0,0 +1,4 @@
+const Profile=()=>{
+    return <><h1>Welcome CH.Solomon Sanjiv</h1><p>Student at CMRIT</p></>
+};
+export default Profile;
